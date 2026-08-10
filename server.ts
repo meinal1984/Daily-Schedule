022667a484575f9f5d1caf623c75926447a0229e
Mrinal Kanti Roy <146665069+meinal1984@users.noreply.github.com>
2026-08-10 10:27:15 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
-import { createServer as createViteServer } from "vite";
 import { GoogleGenAI } from "@google/genai";
 
 const app = express();
